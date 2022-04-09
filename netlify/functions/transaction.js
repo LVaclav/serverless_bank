aa857eab6c11ff47362739a426d523ee85b0f41e
Marshamallow37 <Liam@cervenka.space>
2022-04-10 00:49:48 +0800

    Send money functionality
    les go

--- netlify/functions/transaction.js
+++ netlify/functions/transaction.js
@@ -9,10 +9,20 @@ exports.handler = async (event, context) => {
             'authorization': 'Bearer up:yeah:BYET2CR0T5qqYHBGlOatubPzE8atF2uG4H7DglYl2mVaie5ICYMxca5bQBxTR4NbDWPI7KbuzkAsOtAs4O4RLSCPQjMDkhdQ3fRXFGtwY70q148VUuTXWpMyjY6Du0jg'
         }});
 
+    
+    let payload = {}
+    payload.value = trans.data.data[0].attributes.amount.value
+    payload.description = trans.data.data[0].attributes.description
+    payload.createdAt = trans.data.data[0].attributes.createdAt
+    payload.message = trans.data.data[0].attributes.message
+    payload.rawText = trans.data.data[0].attributes.rawText
+
+    
+    
     return {
         statusCode: 200,
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(trans.data.data[0].attributes)
+        body: JSON.stringify(payload)
         
     }
     
