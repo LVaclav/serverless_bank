7b91643a1243ad43b1711e3b847a8c5d6f902dc6
Marshamallow37 <Liam@cervenka.space>
2022-04-09 17:03:07 +0800

    Real time updates
    and pay id

--- netlify/functions/webhooks.js
+++ netlify/functions/webhooks.js
@@ -1,7 +1,22 @@
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "1382870",
+  key: "e97d9b08c41bd951e219",
+  secret: "1b884675e10fd3461467",
+  cluster: "ap1",
+  useTLS: true
+});
+
+
+
 exports.handler = async (event, context) => {
     try {
       const bodytext = (event.body)
       console.log(bodytext)
+      pusher.trigger("bankauth", "my-event", {
+        message: bodytext
+      });
       return {
         statusCode: 200
       }
@@ -9,4 +24,7 @@ exports.handler = async (event, context) => {
       return { statusCode: 500, body: err.toString() }
     }
   }
+
+  
   // webhook -> https://8a79-120-16-236-208.ngrok.io/
+  // webhook -> http://5b45-106-68-8-46.ngrok.io
