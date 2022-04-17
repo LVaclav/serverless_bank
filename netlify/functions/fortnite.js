cd206119b4459c3cbf29469e575782a523763d34
Marshamallow37 <Liam@cervenka.space>
2022-04-17 11:08:52 +0800

    fortnite kilsl iplewmented

--- netlify/functions/fortnite.js
+++ netlify/functions/fortnite.js
@@ -3,7 +3,7 @@ exports.handler = async (event, context) => {
 
     const axios = require('axios')
     
-    const bryn = await axios.get('https://fortnite-api.com/v2/stats/br/v2/26bad666e95e430eb5b59eaca5c54a2f',{
+    const bryn = await axios.get('https://fortnite-api.com/v2/stats/br/v2/ea5b3afe64204e88b427810cc4445aec',{
         headers: {
             'authorization': 'b7ae0128-5100-48f1-af18-69858ea3d0c7'
         }});
